86dd4f83e80428c929dc704c80a741fdf07a5458
Saitama909 <zekromthanos@gmail.com>
2024-08-06 23:25:33 +1000

    added alarmDisplay

--- Desn2k Project/Core/Src/stopwatch.c
+++ Desn2k Project/Core/Src/stopwatch.c
@@ -13,12 +13,19 @@
 volatile uint32_t seconds = 0;
 int stopwatchRunning = 0;
 uint32_t prevseconds;
+uint32_t lapTimes[2] = {0, 0}; // Array to store up to two lap times
+int lapCount = 0; // Count of laps taken
+int stopwatchStart;
 
 void toggleStopwatch(void);
+void updateLapDisplay();
+void lapStopwatch(void);
 
 // main stopwatch function
 void EnterStopwatch() {
 	LCD_Reset();
+	LCD_SendString("S-watch");
+	stopwatchStart = 1;
 	updateLCD(seconds);
 	prevseconds = -1;
 	while(1) {
@@ -27,8 +34,12 @@ void EnterStopwatch() {
 			toggleStopwatch();
 		} if (input == '*') {
 			resetStopwatch();
+			LCD_SendString("S-watch");
+			stopwatchStart = 1;
+			updateLCD(seconds);
+		} if (input == '0') {
+			lapStopwatch();
 		}
-		updateLCD(seconds);
 		if (stopwatchRunning) {
 			updateLCD(seconds);
 			HAL_GPIO_WritePin(GPIOA, LD2_Pin, RESET);
@@ -50,13 +61,18 @@ void updateLCD(uint32_t count) {
     uint32_t hours = total_seconds / 3600;
     uint32_t minutes = (total_seconds % 3600) / 60;
     uint32_t seconds_LCD = total_seconds % 60;
-    if (prevseconds == seconds_LCD) {
+    if (prevseconds == seconds_LCD && !stopwatchStart) {
     		return;
 	}
     prevseconds = seconds_LCD;
     snprintf(buffer, sizeof(buffer), "%02lu:%02lu:%02lu", hours, minutes, seconds_LCD);
-    LCD_SetCursor(0, 0);
+    LCD_SetCursor(0, 8);
     LCD_SendString(buffer);
+
+    // only runs once at the start of entering stopwatch
+    if (stopwatchStart) {
+    	stopwatchStart = 0;
+    }
 }
 
 // toggles the stopwatch
@@ -73,5 +89,34 @@ void resetStopwatch(void) {
     HAL_TIM_Base_Stop_IT(&htim6);
     seconds = 0;
     stopwatchRunning = false;
+    lapTimes[1] = 0;
+	lapTimes[2] = 0;
+	lapCount = 0;
+	LCD_Clear();
 }
 
+// lap functionality
+void lapStopwatch(void) {
+	// only allows 2 laps
+    if (lapCount < 2) {
+        lapTimes[lapCount] = seconds;
+        lapCount++;
+    }
+    updateLapDisplay();
+}
+
+// update the lap screen
+void updateLapDisplay() {
+    if (lapCount > 0) {
+        char lapBuffer[9];
+        snprintf(lapBuffer, sizeof(lapBuffer), "%02lu:%02lu:%02lu", lapTimes[0] / 3600, (lapTimes[0] % 3600) / 60, lapTimes[0] % 60);
+        LCD_SetCursor(1, 0);
+        LCD_SendString(lapBuffer);
+    }
+    if (lapCount > 1) {
+        char lapBuffer[9];
+        snprintf(lapBuffer, sizeof(lapBuffer), "%02lu:%02lu:%02lu", lapTimes[1] / 3600, (lapTimes[1] % 3600) / 60, lapTimes[1] % 60);
+        LCD_SetCursor(1, 8);
+        LCD_SendString(lapBuffer);
+    }
+}
